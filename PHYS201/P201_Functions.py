76f4a8309319e7d0c5095598ae005944c4b2047c
Edward Brash <brash@jlab.org>
2021-07-10 08:38:00 -0400

    Updates

--- PHYS201/P201_Functions.py
+++ PHYS201/P201_Functions.py
@@ -5,14 +5,14 @@ from sklearn import linear_model
 
 reg = linear_model.LinearRegression()
 
-def linear_fit_plot(x_data,y_data,x_low="",x_high=""):
+def linear_fit_plot(x_data,y_data,x_low="",x_high="",labelstring="Linear Fit"):
     if x_low=="":
         # Takes the x and y values to make a trendline
         reg.fit(x_data,y_data)
         print ('Linear Fit: slope = ',reg.coef_)
         print ('Linear Fit: y_intercept = ',reg.intercept_)
         y_fit = x_data*reg.coef_+reg.intercept_
-        plt.plot(x_data,y_fit,label='Linear Fit')
+        plt.plot(x_data,y_fit,'r-',label='Linear Fit')
         return reg.intercept_,reg.coef_
     else:
         if x_high=="":
@@ -28,10 +28,10 @@ def linear_fit_plot(x_data,y_data,x_low="",x_high=""):
             x_data_cut = np.array(x_data_cut)
             y_data_cut = np.array(y_data_cut)
             reg.fit(x_data_cut,y_data_cut)
-            print ('Linear Fit: slope = ',reg.coef_)
-            print ('Linear Fit: y_intercept = ',reg.intercept_)
+            print ('%s: slope = %0.5f' % (labelstring,reg.coef_))
+            print ('%s: y_intercept = %0.5f' % (labelstring,reg.intercept_))
             y_fit = x_data_cut*reg.coef_+reg.intercept_
-            plt.plot(x_data_cut,y_fit,label='Linear Fit')
+            plt.plot(x_data_cut,y_fit,'r-',label=labelstring)
             
             return reg.intercept_,reg.coef_
 
@@ -40,7 +40,9 @@ from scipy.optimize import curve_fit
 def linearfitfunction(x,*paramlist):
     return paramlist[0]+paramlist[1]*x
 
-def linear_fit_plot_errors_core(xi,yi):
+def linear_fit_plot_errors_core(xi,yi,labelstring="Linear Fit",linestring="r-"):
+
+    linestring2 = linestring+"-"
     
     init_vals = [0.0 for x in range(2)]
     popt, pcov = curve_fit(linearfitfunction,xi,yi,p0=init_vals)
@@ -53,26 +55,27 @@ def linear_fit_plot_errors_core(xi,yi):
     upper = np.percentile(ysample,97.5,axis=0)
     middle = (lower+upper)/2.0
 
-    print("Coefficients (from curve_fit)")
+    print("%s: Coefficients (from curve_fit)" % labelstring)
     print (popt)
-    print("Covariance Matrix (from curve_fit)")
+    print("%s: Covariance Matrix (from curve_fit)" % labelstring)
     print (pcov)
 
     print()
-    print ("Final Result: y = (%0.2f +/- %0.2f) x + (%0.2f +/- %0.2f)" % (popt[1],perr[1],popt[0],perr[0]))
+    print ("%s: Final Result: y = (%0.5f +/- %0.5f) x + (%0.5f +/- %0.5f)" % (labelstring,popt[1],perr[1],popt[0],perr[0]))
+    print()
 
     #plt.plot(xi,yi,'o')
 
-    plt.plot(xi,middle,label='Linear Fit')
-    plt.plot(xi,lower)
-    plt.plot(xi,upper)
+    plt.plot(xi,middle,linestring,label=labelstring,linewidth=1)
+    plt.plot(xi,lower,linestring2,linewidth=1)
+    plt.plot(xi,upper,linestring2,linewidth=1)
 
     return popt[1],perr[1],popt[0],perr[0]
 
-def linear_fit_plot_errors(xi,yi,x_low="",x_high=""):
+def linear_fit_plot_errors(xi,yi,x_low="",x_high="",labelstring="Linear Fit",linestring="r-"):
     if x_low=="":
         # Takes the x and y values to make a trendline
-        intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(xi,yi)
+        intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(xi,yi,labelstring,linestring)
         return intercept,slope,dintercept,slope
     else:
         if x_high=="":
@@ -88,5 +91,5 @@ def linear_fit_plot_errors(xi,yi,x_low="",x_high=""):
             x_data_cut = np.array(x_data_cut)
             y_data_cut = np.array(y_data_cut)
             # Takes the x and y values to make a trendline
-            intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(x_data_cut,y_data_cut)
+            intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(x_data_cut,y_data_cut,labelstring,linestring)
             return intercept,slope,dintercept,slope
