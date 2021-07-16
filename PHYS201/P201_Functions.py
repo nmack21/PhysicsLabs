0e3f37324edac91e63fb65714bb5afb21d65a28b
Edward Brash <brash@jlab.org>
2021-07-15 22:33:08 -0400

    update

--- PHYS201/P201_Functions.py
+++ PHYS201/P201_Functions.py
@@ -40,41 +40,6 @@ from scipy.optimize import curve_fit
 def linearfitfunction(x,*paramlist):
     return paramlist[0]+paramlist[1]*x
 
-def quadraticfitfunction(x,*paramlist):
-    return paramlist[0]+paramlist[1]*x+paramlist[2]*x*x
-
-def quadratic_fit_plot_errors_core(xi,yi,labelstring="Quadratic Fit",linestring="r-"):
-
-    linestring2 = linestring+"-"
-    
-    init_vals = [0.0 for x in range(3)]
-    popt, pcov = curve_fit(quadraticfitfunction,xi,yi,p0=init_vals)
-    perr = np.sqrt(np.diag(pcov))
-
-    ps = np.random.multivariate_normal(popt,pcov,10000)
-    ysample=np.asarray([quadraticfitfunction(xi,*pi) for pi in ps])
-
-    lower = np.percentile(ysample,2.5,axis=0)
-    upper = np.percentile(ysample,97.5,axis=0)
-    middle = (lower+upper)/2.0
-
-    print("%s: Coefficients (from curve_fit)" % labelstring)
-    print (popt)
-    print("%s: Covariance Matrix (from curve_fit)" % labelstring)
-    print (pcov)
-
-    print()
-    print ("%s: Final Result: y = (%0.5f +/- %0.5f) x^2 + (%0.5f +/- %0.5f) x + (%0.5f +/- %0.5f)" %(labelstring,popt[2],perr[2],popt[1],perr[1],popt[0],perr[0]))
-    print()
-
-    #plt.plot(xi,yi,'o')
-
-    plt.plot(xi,middle,linestring,label=labelstring,linewidth=1)
-    plt.plot(xi,lower,linestring2,linewidth=1)
-    plt.plot(xi,upper,linestring2,linewidth=1)
-
-    return popt[2],popt[1],popt[0],perr[2],perr[1],perr[0]
-
 def linear_fit_plot_errors_core(xi,yi,labelstring="Linear Fit",linestring="r-"):
 
     linestring2 = linestring+"-"
@@ -128,6 +93,41 @@ def linear_fit_plot_errors(xi,yi,x_low="",x_high="",labelstring="Linear Fit",lin
             # Takes the x and y values to make a trendline
             intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(x_data_cut,y_data_cut,labelstring,linestring)
             return intercept,slope,dintercept,dslope
+        
+def quadraticfitfunction(x,*paramlist):
+    return paramlist[0]+paramlist[1]*x+paramlist[2]*x*x
+        
+def quadratic_fit_plot_errors_core(xi,yi,labelstring="Quadratic Fit",linestring="r-"):
+
+    linestring2 = linestring+"-"
+    
+    init_vals = [0.0 for x in range(3)]
+    popt, pcov = curve_fit(quadraticfitfunction,xi,yi,p0=init_vals)
+    perr = np.sqrt(np.diag(pcov))
+
+    ps = np.random.multivariate_normal(popt,pcov,10000)
+    ysample=np.asarray([quadraticfitfunction(xi,*pi) for pi in ps])
+
+    lower = np.percentile(ysample,2.5,axis=0)
+    upper = np.percentile(ysample,97.5,axis=0)
+    middle = (lower+upper)/2.0
+
+    print("%s: Coefficients (from curve_fit)" % labelstring)
+    print (popt)
+    print("%s: Covariance Matrix (from curve_fit)" % labelstring)
+    print (pcov)
+
+    print()
+    print ("%s: Final Result: y = (%0.5f +/- %0.5f) x^2 + (%0.5f +/- %0.5f) x + (%0.5f +/- %0.5f)" %(labelstring,popt[2],perr[2],popt[1],perr[1],popt[0],perr[0]))
+    print()
+
+    #plt.plot(xi,yi,'o')
+
+    plt.plot(xi,middle,linestring,label=labelstring,linewidth=1)
+    plt.plot(xi,lower,linestring2,linewidth=1)
+    plt.plot(xi,upper,linestring2,linewidth=1)
+
+    return popt[2],popt[1],popt[0],perr[2],perr[1],perr[0]
            
 def quadratic_fit_plot_errors(xi,yi,x_low="",x_high="",labelstring="Quadratic Fit",linestring="r-"):
     if x_low=="":
