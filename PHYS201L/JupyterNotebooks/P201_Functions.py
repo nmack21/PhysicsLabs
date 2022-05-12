32730a7a473bb56774c8ca68627c738028f46d6f
Edward Brash <brash@jlab.org>
2022-05-12 11:53:13 -0400

    Updates

--- PHYS201L/JupyterNotebooks/P201_Functions.py
+++ PHYS201L/JupyterNotebooks/P201_Functions.py
@@ -5,42 +5,12 @@ from sklearn import linear_model
 
 reg = linear_model.LinearRegression()
 
-def linear_fit_plot(x_data,y_data,x_low="",x_high="",labelstring="Linear Fit"):
-    if x_low=="":
-        # Takes the x and y values to make a trendline
-        reg.fit(x_data,y_data)
-        print ('Linear Fit: slope = ',reg.coef_)
-        print ('Linear Fit: y_intercept = ',reg.intercept_)
-        y_fit = x_data*reg.coef_+reg.intercept_
-        plt.plot(x_data,y_fit,'r-',label='Linear Fit')
-        return reg.intercept_,reg.coef_
-    else:
-        if x_high=="":
-            print ('Missing x_high parameter!!')
-            return -1000,-1000
-        else:
-            x_data_cut = []
-            y_data_cut = []
-            for i in range(len(x_data)):
-                if x_data[i]>=float(x_low) and x_data[i]<=float(x_high):
-                    x_data_cut.append(x_data[i])
-                    y_data_cut.append(y_data[i])
-            x_data_cut = np.array(x_data_cut)
-            y_data_cut = np.array(y_data_cut)
-            reg.fit(x_data_cut,y_data_cut)
-            print ('%s: slope = %0.5f' % (labelstring,reg.coef_))
-            print ('%s: y_intercept = %0.5f' % (labelstring,reg.intercept_))
-            y_fit = x_data_cut*reg.coef_+reg.intercept_
-            plt.plot(x_data_cut,y_fit,'r-',label=labelstring)
-            
-            return reg.intercept_,reg.coef_
-
 from scipy.optimize import curve_fit
 
 def linearfitfunction(x,*paramlist):
     return paramlist[0]+paramlist[1]*x
 
-def linear_fit_plot_errors_core(xi,yi,labelstring="Linear Fit",linestring="r-"):
+def linear_fit_plot_core(xi,yi,labelstring="Linear Fit",linestring="r-",plot_name=plt):
 
     linestring2 = linestring+"-"
     
@@ -66,16 +36,16 @@ def linear_fit_plot_errors_core(xi,yi,labelstring="Linear Fit",linestring="r-"):
 
     #plt.plot(xi,yi,'o')
 
-    plt.plot(xi,middle,linestring,label=labelstring,linewidth=1)
-    plt.plot(xi,lower,linestring2,linewidth=1)
-    plt.plot(xi,upper,linestring2,linewidth=1)
+    plot_name.plot(xi,middle,linestring,label=labelstring,linewidth=1)
+    plot_name.plot(xi,lower,linestring2,linewidth=1)
+    plot_name.plot(xi,upper,linestring2,linewidth=1)
 
     return popt[0],popt[1],perr[0],perr[1]
 
-def linear_fit_plot_errors(xi,yi,x_low="",x_high="",labelstring="Linear Fit",linestring="r-"):
+def linear_fit_plot(xi,yi,plot_name,x_low="",x_high="",labelstring="Linear Fit",linestring="r-"):
     if x_low=="":
         # Takes the x and y values to make a trendline
-        intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(xi,yi,labelstring,linestring)
+        intercept,slope,dintercept,dslope = linear_fit_plot_core(xi,yi,labelstring,linestring)
         return intercept,slope,dintercept,dslope
     else:
         if x_high=="":
@@ -91,13 +61,70 @@ def linear_fit_plot_errors(xi,yi,x_low="",x_high="",labelstring="Linear Fit",lin
             x_data_cut = np.array(x_data_cut)
             y_data_cut = np.array(y_data_cut)
             # Takes the x and y values to make a trendline
-            intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(x_data_cut,y_data_cut,labelstring,linestring)
+            intercept,slope,dintercept,dslope = linear_fit_plot_core(x_data_cut,y_data_cut,labelstring,linestring,plot_name)
+            return intercept,slope,dintercept,dslope
+        
+def linear_fit_plot_errors_core(xi,yi,sigmai,labelstring="Linear Fit",linestring="r-",plot_name=plt):
+
+    linestring2 = linestring+"-"
+    
+    init_vals = [0.0 for x in range(2)]
+    popt, pcov = curve_fit(linearfitfunction,xi,yi,p0=init_vals,sigma=sigmai)
+    perr = np.sqrt(np.diag(pcov))
+
+    ps = np.random.multivariate_normal(popt,pcov,10000)
+    ysample=np.asarray([linearfitfunction(xi,*pi) for pi in ps])
+
+    lower = np.percentile(ysample,2.5,axis=0)
+    upper = np.percentile(ysample,97.5,axis=0)
+    middle = (lower+upper)/2.0
+
+    print("%s: Coefficients (from curve_fit)" % labelstring)
+    print (popt)
+    print("%s: Covariance Matrix (from curve_fit)" % labelstring)
+    print (pcov)
+
+    print()
+    print ("%s: Final Result: y = (%0.5f +/- %0.5f) x + (%0.5f +/- %0.5f)" % (labelstring,popt[1],perr[1],popt[0],perr[0]))
+    print()
+
+    #plt.plot(xi,yi,'o')
+
+    plot_name.plot(xi,middle,linestring,label=labelstring,linewidth=1)
+    plot_name.plot(xi,lower,linestring2,linewidth=1)
+    plot_name.plot(xi,upper,linestring2,linewidth=1)
+
+    return popt[0],popt[1],perr[0],perr[1]
+
+def linear_fit_plot_errors(xi,yi,sigmai,plot_name,x_low="",x_high="",labelstring="Linear Fit",linestring="r-"):
+    if x_low=="":
+        # Takes the x and y values to make a trendline
+        intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(xi,yi,sigmai,labelstring,linestring)
+        return intercept,slope,dintercept,dslope
+    else:
+        if x_high=="":
+            print ('Missing x_high parameter!!')
+            return -1000,-1000,-1000,-1000
+        else:
+            x_data_cut = []
+            y_data_cut = []
+            sigmai_cut = []
+            for i in range(len(xi)):
+                if xi[i]>=float(x_low) and xi[i]<=float(x_high):
+                    x_data_cut.append(xi[i])
+                    y_data_cut.append(yi[i])
+                    sigmai_cut.append(sigmai[i])
+            x_data_cut = np.array(x_data_cut)
+            y_data_cut = np.array(y_data_cut)
+            sigmai_cut = np.array(sigmai_cut)
+            # Takes the x and y values to make a trendline
+            intercept,slope,dintercept,dslope = linear_fit_plot_errors_core(x_data_cut,y_data_cut,sigmai_cut,labelstring,linestring,plot_name)
             return intercept,slope,dintercept,dslope
         
 def quadraticfitfunction(x,*paramlist):
     return paramlist[0]+paramlist[1]*x+paramlist[2]*x*x
         
-def quadratic_fit_plot_errors_core(xi,yi,labelstring="Quadratic Fit",linestring="r-"):
+def quadratic_fit_plot_core(xi,yi,labelstring="Quadratic Fit",linestring="r-",plot_name=plt):
 
     linestring2 = linestring+"-"
     
@@ -123,16 +150,16 @@ def quadratic_fit_plot_errors_core(xi,yi,labelstring="Quadratic Fit",linestring=
 
     #plt.plot(xi,yi,'o')
 
-    plt.plot(xi,middle,linestring,label=labelstring,linewidth=1)
-    plt.plot(xi,lower,linestring2,linewidth=1)
-    plt.plot(xi,upper,linestring2,linewidth=1)
+    plot_name.plot(xi,middle,linestring,label=labelstring,linewidth=1)
+    plot_name.plot(xi,lower,linestring2,linewidth=1)
+    plot_name.plot(xi,upper,linestring2,linewidth=1)
 
     return popt[2],popt[1],popt[0],perr[2],perr[1],perr[0]
            
-def quadratic_fit_plot_errors(xi,yi,x_low="",x_high="",labelstring="Quadratic Fit",linestring="r-"):
+def quadratic_fit_plot(xi,yi,plot_name,x_low="",x_high="",labelstring="Quadratic Fit",linestring="r-"):
     if x_low=="":
         # Takes the x and y values to make a trendline
-        a,b,c,da,db,dc = quadratic_fit_plot_errors_core(xi,yi,labelstring,linestring)
+        a,b,c,da,db,dc = quadratic_fit_plot_core(xi,yi,labelstring,linestring,plot_name)
         return a,b,c,da,db,da
     else:
         if x_high=="":
@@ -148,8 +175,66 @@ def quadratic_fit_plot_errors(xi,yi,x_low="",x_high="",labelstring="Quadratic Fi
             x_data_cut = np.array(x_data_cut)
             y_data_cut = np.array(y_data_cut)
             # Takes the x and y values to make a trendline
-            a,b,c,da,db,dc = quadratic_fit_plot_errors_core(x_data_cut,y_data_cut,labelstring,linestring)
+            a,b,c,da,db,dc = quadratic_fit_plot_core(x_data_cut,y_data_cut,labelstring,linestring,plot_name)
             return a,b,c,da,db,dc
+        
+def quadratic_fit_plot_errors_core(xi,yi,sigmai,labelstring="Quadratic Fit",linestring="r-",plot_name=plt):
+
+    linestring2 = linestring+"-"
+    
+    init_vals = [0.0 for x in range(3)]
+    popt, pcov = curve_fit(quadraticfitfunction,xi,yi,p0=init_vals,sigma=sigmai)
+    perr = np.sqrt(np.diag(pcov))
+
+    ps = np.random.multivariate_normal(popt,pcov,10000)
+    ysample=np.asarray([quadraticfitfunction(xi,*pi) for pi in ps])
+
+    lower = np.percentile(ysample,2.5,axis=0)
+    upper = np.percentile(ysample,97.5,axis=0)
+    middle = (lower+upper)/2.0
+
+    print("%s: Coefficients (from curve_fit)" % labelstring)
+    print (popt)
+    print("%s: Covariance Matrix (from curve_fit)" % labelstring)
+    print (pcov)
+
+    print()
+    print ("%s: Final Result: y = (%0.5f +/- %0.5f) x^2 + (%0.5f +/- %0.5f) x + (%0.5f +/- %0.5f)" %(labelstring,popt[2],perr[2],popt[1],perr[1],popt[0],perr[0]))
+    print()
+
+    #plt.plot(xi,yi,'o')
+
+    plot_name.plot(xi,middle,linestring,label=labelstring,linewidth=1)
+    plot_name.plot(xi,lower,linestring2,linewidth=1)
+    plot_name.plot(xi,upper,linestring2,linewidth=1)
+
+    return popt[2],popt[1],popt[0],perr[2],perr[1],perr[0]
+           
+def quadratic_fit_plot_errors(xi,yi,sigmai,plot_name,x_low="",x_high="",labelstring="Quadratic Fit",linestring="r-"):
+    if x_low=="":
+        # Takes the x and y values to make a trendline
+        a,b,c,da,db,dc = quadratic_fit_plot_core(xi,yi,sigmai,labelstring,linestring,plot_name)
+        return a,b,c,da,db,da
+    else:
+        if x_high=="":
+            print ('Missing x_high parameter!!')
+            return -1000,-1000,-1000,-1000,-1000,-1000
+        else:
+            x_data_cut = []
+            y_data_cut = []
+            sigmai_cut = []
+            for i in range(len(xi)):
+                if xi[i]>=float(x_low) and xi[i]<=float(x_high):
+                    x_data_cut.append(xi[i])
+                    y_data_cut.append(yi[i])
+                    sigmai_cut.append(sigmai[i])
+            x_data_cut = np.array(x_data_cut)
+            y_data_cut = np.array(y_data_cut)
+            sigmai_cut = np.array(sigmai_cut)
+            # Takes the x and y values to make a trendline
+            a,b,c,da,db,dc = quadratic_fit_plot_core(x_data_cut,y_data_cut,sigmai_cut,labelstring,linestring,plot_name)
+            return a,b,c,da,db,dc
+
 
 def set_dark_mode(dark_mode = True):
     if (dark_mode):
